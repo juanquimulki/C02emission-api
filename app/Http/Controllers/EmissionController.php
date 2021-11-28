186ed58f1f4d9790bd94b98c4c711b9d8c8e6df2
Juanqui Mulki <juanquimulki@hotmail.com>
2021-11-27 22:40:46 -0300

    Returns value

--- app/Http/Controllers/EmissionController.php
+++ app/Http/Controllers/EmissionController.php
@@ -3,22 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
 use App\Models\State as State;
 
 
 class EmissionController extends Controller
 {
-    public function read() {
+    public function read(Request $request) {
         $stateObj = new State();
-        $stateQuery = $stateObj->getStates()["Hawaii"];
+        $stateQuery = $stateObj->getStates()[$request->state];
 
         $requestUrl  = env("HTTP_URL");
         $parameters = ["{%a}", "{%s}"];
         $data   = [env("API_KEY"), $stateQuery];
         
         $requestUrl = str_replace($parameters, $data, $requestUrl);
-
         $response = Http::get($requestUrl);
-        echo json_encode($response->json());
+
+        $arrayData = $response->json()["series"][0]["data"];
+
+        $sum = 0;
+        for ($i = env("YEAR_MAX") - $request->from; $i >= env("YEAR_MAX") - $request->to; $i--) {
+            $sum += $arrayData[$i][1];
+        }
+
+        echo round($sum,1) . " million";
     }
 }
